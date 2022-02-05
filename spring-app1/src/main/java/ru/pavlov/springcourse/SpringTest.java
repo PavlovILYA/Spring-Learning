d59fd06dea9f2762992e559ad7bc6e91170bf045
Илюша <79216023478@yandex.ru>
2022-02-05 18:55:05 +0300

    several @Component-beans

--- spring-app1/src/main/java/ru/pavlov/springcourse/SpringTest.java
+++ spring-app1/src/main/java/ru/pavlov/springcourse/SpringTest.java
@@ -8,11 +8,15 @@ public class SpringTest {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("musicBean", Music.class);
+        Music music = context.getBean("rapMusic", Music.class);
+        Music music2 = context.getBean("rockMusic", Music.class);
+        Music music3 = context.getBean("classicalMusic", Music.class);
 
         MusicPlayer musicPlayer = MusicPlayer.getMusicPlayer();
 
         musicPlayer.addMusic(music);
+        musicPlayer.addMusic(music2);
+        musicPlayer.addMusic(music3);
         musicPlayer.playMusic();
 
         context.close();
