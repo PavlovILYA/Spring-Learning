2280558243db8d7cea0b2de1aa272d62673702c5
Илюша <79216023478@yandex.ru>
2022-02-05 14:46:06 +0300

    add list property

--- spring-app1/src/main/java/ru/pavlov/springcourse/MusicPlayer.java
+++ spring-app1/src/main/java/ru/pavlov/springcourse/MusicPlayer.java
@@ -1,23 +1,26 @@
 package ru.pavlov.springcourse;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
     private String name;
     private int volume;
 
     // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(List<Music> musics) {
+        this.musicList = musics;
     }
 
     public MusicPlayer() {}
 
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList)
+            System.out.println("Playing: " + music.getSong());
     }
 
     public void setName(String name) {
