50862514853a7e8a5dc72ae5436bdc967ba4889c
Илюша <79216023478@yandex.ru>
2022-02-05 12:34:39 +0300

    rebase project structure

--- spring-app1/src/main/java/ru/pavlov/springcourse/SpringTest.java
+++ spring-app1/src/main/java/ru/pavlov/springcourse/SpringTest.java
@@ -2,7 +2,7 @@ package ru.pavlov.springcourse;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class TestSpring {
+public class SpringTest {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
--- spring-app1/src/main/java/ru/pavlov/springcourse/TestBean.java
+++ spring-app1/src/main/java/ru/pavlov/springcourse/TestBean.java
@@ -7,11 +7,11 @@ public class TestBean {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }
