747c4cb6d3be190dcbb04c23447d24f8faf6cdf3
Илюша <79216023478@yandex.ru>
2022-02-06 16:29:01 +0300

    completely understand spring configuration by java-code

--- spring-app1/src/main/java/ru/pavlov/springcourse/MusicPlayer.java
+++ spring-app1/src/main/java/ru/pavlov/springcourse/MusicPlayer.java
@@ -4,48 +4,30 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
-
-
 public class MusicPlayer {
-    private HashMap<MusicType, Music> musicHashMap = new HashMap();
+    private HashMap<MusicType, Music> musicHashMap = new HashMap<>();
     @Value("${musicPlayer.name}")
     private String name;
     @Value("${musicPlayer.volume}")
     private int volume;
 
-    static public MusicPlayer getMusicPlayer() {
-        return new MusicPlayer();
-    }
-
-    // IoC
-    private MusicPlayer(HashMap<MusicType, Music> musics) {
-        this.musicHashMap = musics;
-    }
-
-    public MusicPlayer(@Qualifier("classicalMusic") Music classicalMusic,
-                        @Qualifier("rockMusic") Music rockMusic,
-                        @Qualifier("rapMusic") Music rapMusic) {
-        musicHashMap.put(MusicType.CLASSICAL, classicalMusic);
-        musicHashMap.put(MusicType.ROCK, rockMusic);
-        musicHashMap.put(MusicType.RAP, rapMusic);
-    }
-
-    private MusicPlayer() {}
-
-//    public void setMusicList(List<Music> musicList) {
-//        this.musicList = musicList;
+//    public MusicPlayer(@Qualifier("classicalMusic") Music classicalMusic,
+//                        @Qualifier("rockMusic") Music rockMusic,
+//                        @Qualifier("rapMusic") Music rapMusic) {
+//        musicHashMap.put(MusicType.CLASSICAL, classicalMusic);
+//        musicHashMap.put(MusicType.ROCK, rockMusic);
+//        musicHashMap.put(MusicType.RAP, rapMusic);
 //    }
+
+    public MusicPlayer(HashMap<MusicType, Music> musicHashMap) {
+        this.musicHashMap = musicHashMap;
+    }
 
     public String playMusic(MusicType musicType) {
         return musicHashMap.get(musicType).getSong();
     }
-
-//    public void addMusic(Music music) {
-//        musicList.add(music);
-//    }
 
     public String getName() {
         return name;
--- spring-app1/src/main/java/ru/pavlov/springcourse/SpringTest.java
+++ spring-app1/src/main/java/ru/pavlov/springcourse/SpringTest.java
@@ -7,18 +7,18 @@ public class SpringTest {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
                 MyCoolSpringConfig.class
         );
+        Computer comp = context.getBean("computer", Computer.class);
 
-        //Computer comp = context.getBean("computer", Computer.class);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(comp.turnOnMusic(MusicType.RAP));
 
-//        System.out.println(comp.turnOnMusic(MusicType.CLASSICAL));
-        System.out.println(musicPlayer.getVolume());
-        System.out.println(musicPlayer.getName());
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        RapMusic rapMusic = context.getBean("rapMusic", RapMusic.class);
-        RapMusic rapMusic2 = context.getBean("rapMusic", RapMusic.class);
-        System.out.println(rapMusic == rapMusic2);
+//        System.out.println(musicPlayer.getVolume());
+//        System.out.println(musicPlayer.getName());
 
+//        RapMusic rapMusic = context.getBean("rapMusic", RapMusic.class);
+//        RapMusic rapMusic2 = context.getBean("rapMusic", RapMusic.class);
+//        System.out.println(rapMusic == rapMusic2);
 
         context.close();
     }
